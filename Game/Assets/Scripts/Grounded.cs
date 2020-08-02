8614a5b0c97ac23c28dd133a0e22cacb2d7478cf
codethecoffee <sujunglee2020@u.northwestern.edu>
2020-08-03 00:02:16 +0900

    Implement left and right movement plus jump functionality. Fixed bug where you could jump in the air

---
+++ Game/Assets/Scripts/Grounded.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grounded : MonoBehaviour
+{
+    GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = gameObject.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            Player.GetComponent<MovePlayer>().isGrounded = true;
+        }
+    }
+
+    // When 
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            Player.GetComponent<MovePlayer>().isGrounded = false;
+        }
+        
+    }
+}
---
+++ Game/Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    // Flag indicating whether player is on the ground
+    public bool isGrounded = true;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Jump();
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+    }
+
+    void Jump() {
+        if (Input.GetButtonDown("Jump") && isGrounded == true) {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+        }
+    }
+}
