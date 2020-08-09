0b06b2f3d9aa07a44de38d4d3abe2f482186062d
mtruong68 <marytruong2020@u.northwestern.edu>
2020-08-09 15:56:55 -0500

    script for camera to follow the player & not exceed bounds + made the scene longer for testing

---
+++ Game/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public Transform player;
+    public float xstart;
+    public float xend;
+    public float ystart;
+    public float yend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        float xpos = player.position.x;
+        float ypos = player.position.y;
+        //determine bounds for the camera in the editor
+        if (xpos < xstart) xpos = xstart;
+        if (xpos > xend) xpos = xend;
+        if (ypos < ystart) ypos = ystart;
+        if (ypos > yend) ypos = yend;
+
+        
+      transform.position = new Vector3(xpos, ypos, -1);
+    }
+}
