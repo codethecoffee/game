5773ca48d250be276f76ee95c4a6e7b909ee72ae
mtruong68 <marytruong2020@u.northwestern.edu>
2020-08-19 20:12:05 -0500

    player and enemy now have lives... rudimentary way to keep track of player attacks on enemies

---
+++ Game/Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    //default 2 lives
+    private int _lives = 2;
+
+    private void Update()
+    {
+        if (_lives <= 0)
+        {
+            EnemyDeath();
+        }
+    }
+
+    public void LoseLife()
+    {
+        _lives--;
+    }
+
+    public void EnemyDeath()
+    {
+        Destroy(gameObject);
+    }
+}
