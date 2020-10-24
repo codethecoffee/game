e058e2f1e7deb53c385e44314092b37e72878450
codethecoffee <sujunglee2020@u.northwestern.edu>
2020-10-24 21:34:52 +0900

    Add score label that updates in corner

---
+++ Game/Assets/Scripts/UIManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Player player;
+    public Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = "Score: " + player.totalScore.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "Score: " + player.totalScore.ToString();
+    }
+}
